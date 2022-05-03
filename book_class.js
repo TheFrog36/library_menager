aa54c1c1c16aaceeab807e8b9696efc9c8f06641
TheFrog36 <artisangiorgio98@gmail.com>
2022-05-03 09:34:21 +0200

    creation

---
+++ book_class.js
@@ -0,0 +1,26 @@
+class Book{
+    constructor(title,author,genre,price,copies,pages,publication_year,editor,discount){
+        this.title = title
+        this.author = author
+        this.genre = genre
+        this.price = price
+        this.copies = copies
+        this.pages = pages
+        this.publication_year = publication_year
+        this.editor = editor
+        this.discount = discount
+    }
+    //title
+    //author
+    //genre
+    //price
+    //copies
+    //pages
+    //publication_year
+    //editor
+    //discunt
+
+    //toString()
+    //get_public_price() {prende il prezzo di partenza, aggiunge le tasse (10%) +30% - il discount% (preso dal prezzo originale)}
+
+}
---
+++ magazine_class.js
@@ -0,0 +1,21 @@
+class Magazines{
+    constructor(title, release_number, editor, cadence, price, copies, discount){
+        this.title = title
+        this.release_number = release_number
+        this.editor = editor
+        this.cadence = cadence
+        this.price = price
+        this.copies = copies
+        this.discount = discount
+    }
+    //title
+    //release_number
+    //editor
+    //cadence
+    //price
+    //copies
+    //discount
+
+    //toString()
+    //get_public_price() {prende il prezzo di partenza, aggiunge le tasse (20%) +30% - il discount% (preso dal prezzo originale)}
+}
